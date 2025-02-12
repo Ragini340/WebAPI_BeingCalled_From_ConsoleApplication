ed20ca0218809ce001a5fae32c3e30e50b006ca1
Ragini340 <ragini31bxr@gmail.com>
2025-02-12 09:52:31 +0530

    #WebAPIDemo_Updated GreetingsController

--- WebAPIDemo/Controllers/GreetingsController.cs
+++ WebAPIDemo/Controllers/GreetingsController.cs
@@ -12,3 +12,43 @@ namespace WebAPIDemo.Controllers
         }
     }
 }
+
+/*
+ namespace CSharp_Practice.Asynchronous_Programming.Task_Class
+ {
+    //Calling Web API HTTP request from Console Application
+    public class ReturnValueFromTask
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main method started......");
+            Console.WriteLine("Enter the name: ");
+            string Name = Console.ReadLine();
+            Print(Name);
+            Console.WriteLine("Main method end");
+            Console.ReadKey();
+        }
+
+        public async static void Print(string Name)
+        {
+            Console.WriteLine("Print method started......");
+            var GreetingSMessage = await Greetings(Name);
+            Console.WriteLine($"\n{GreetingSMessage}");
+            Console.WriteLine("Print method end");
+        }
+
+        public static async Task<string> Greetings(string Name)
+        {
+            string message = string.Empty;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44357/");
+                HttpResponseMessage response = await client.GetAsync($"api/greetings/{Name}");
+                message = await response.Content.ReadAsStringAsync();
+            }
+            return message;
+        }
+
+    }
+}
+ */
