117c73ea8b5edf7f20fc15e6eaa8166406a2b9c8
Ragini340 <ragini31bxr@gmail.com>
2025-02-12 09:49:15 +0530

    Add project files.

---
+++ WebAPIDemo/Controllers/GreetingsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class GreetingsController : ApiController
+    {
+        [Route("api/greetings/{name}")]
+        [HttpGet]
+        public string GetGreetings(string name)
+        {
+            return $"Hello {name}, Welcome to Web API";
+        }
+    }
+}
